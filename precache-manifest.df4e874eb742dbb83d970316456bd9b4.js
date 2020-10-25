afb2653fdaef69915c76940c30afdfc92f468e71
raz kedem <razkedem1987@gmail.com>
2020-10-25 06:06:34 +0200

    Updates

---
+++ precache-manifest.df4e874eb742dbb83d970316456bd9b4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a0aced5c4a4b423c0e631e2aa745f1",
+    "url": "/Raz-Kedem-21-10-2020/index.html"
+  },
+  {
+    "revision": "d51669e43f3707b4f635",
+    "url": "/Raz-Kedem-21-10-2020/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "1c007419c37e9e2c3445",
+    "url": "/Raz-Kedem-21-10-2020/static/css/main.63f8ad93.chunk.css"
+  },
+  {
+    "revision": "d51669e43f3707b4f635",
+    "url": "/Raz-Kedem-21-10-2020/static/js/2.19bad300.chunk.js"
+  },
+  {
+    "revision": "a07f0d37bad529c29913b02917e371bc",
+    "url": "/Raz-Kedem-21-10-2020/static/js/2.19bad300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c007419c37e9e2c3445",
+    "url": "/Raz-Kedem-21-10-2020/static/js/main.55b71e86.chunk.js"
+  },
+  {
+    "revision": "6ef9ac02a33605a96b61",
+    "url": "/Raz-Kedem-21-10-2020/static/js/runtime-main.2c578467.js"
+  },
+  {
+    "revision": "3ae746566a24db3f8210c17a58c114ff",
+    "url": "/Raz-Kedem-21-10-2020/static/media/bulb-off.3ae74656.svg"
+  },
+  {
+    "revision": "09aced8f375e949228edb6df454c9dbf",
+    "url": "/Raz-Kedem-21-10-2020/static/media/bulb-on.09aced8f.svg"
+  },
+  {
+    "revision": "9bec326a66d4f95b4aa6d44d5f209aa5",
+    "url": "/Raz-Kedem-21-10-2020/static/media/star-full.9bec326a.svg"
+  },
+  {
+    "revision": "1d8177d9c648408f0b155f31d27b393f",
+    "url": "/Raz-Kedem-21-10-2020/static/media/star-ouline.1d8177d9.svg"
+  },
+  {
+    "revision": "ab2e43fb9aff76708498f74a53d3db0b",
+    "url": "/Raz-Kedem-21-10-2020/static/media/temp-dark.ab2e43fb.svg"
+  },
+  {
+    "revision": "60740ca90ae3536de962c960a1f696af",
+    "url": "/Raz-Kedem-21-10-2020/static/media/temp-light.60740ca9.svg"
+  },
+  {
+    "revision": "6958aaf34b2679c5479a99c3030ee1f9",
+    "url": "/Raz-Kedem-21-10-2020/static/media/world.6958aaf3.jpg"
+  }
+]);
